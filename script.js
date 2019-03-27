9ff192520c59fcc10bfbff3e8f20858f91d98ae4
Lewis Bailey <lbaileyjr219@gmail.com>
2019-03-26 20:25:18 -0400

    added variables

---
+++ script.js
@@ -0,0 +1,25 @@
+var redShades = {
+  title:'red',
+  product:'Rock-N-Stroll',
+  img:'images/product-red.jpg',
+  price:'199.99'
+};
+var blackShades = {
+  title:'black',
+  product:'Rock-N-Stroll',
+  img:'images/product-black.jpg',
+  price:'199.99'
+};
+var blueShades = {
+  title:'blue',
+  product:'Rock-N-Stroll',
+  img:'images/product-blue.jpg',
+  price:'199.99'
+};
+var greenShades = {
+  title:'green',
+  product:'Rock-N-Stroll',
+  img:'images/product-green.jpg',
+  price:'199.99'
+};
+var shades = [redShades, blackShades, blueShades, greenShades];
