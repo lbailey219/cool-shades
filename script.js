315141da72070446953a335250d1fa99849d5f46
Lewis Bailey <lbaileyjr219@gmail.com>
2019-03-28 19:56:05 -0400

    js chnges

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 var redShades = {
-  title:'red',
-  product:'Rock-N-Stroll',
+  title:'Red',
+  subtitle:'Rock-N-Stroll',
   img:'images/product-red.jpg',
-  price:'199.99'
+  price:199.99
 };
 var blackShades = {
-  title:'black',
-  product:'Rock-N-Stroll',
+  title:'Black',
+  subtitle:'Rock-N-Stroll',
   img:'images/product-black.jpg',
-  price:'199.99'
+  price:199.99
 };
 var blueShades = {
-  title:'blue',
-  product:'Rock-N-Stroll',
+  title:'Blue',
+  subtitle:'Rock-N-Stroll',
   img:'images/product-blue.jpg',
-  price:'199.99'
+  price:199.99
 };
 var greenShades = {
-  title:'green',
-  product:'Rock-N-Stroll',
+  title:'Green',
+  subtitle:'Rock-N-Stroll',
   img:'images/product-green.jpg',
-  price:'199.99'
+  price:199.99
 };
 var shades = [redShades, blackShades, blueShades, greenShades];
+
+
+function mobileProducts(product) {
+  return `
+      <div class="row">
+        <div class="col">
+          <img src="${product.img}" />
+          <br />
+          <span>${product.subtitle}</span>
+          <h3>${product.title}</h3>
+          <h5>$${product.price}</h5>
+          <button type="button" class="btn btn-shop" "btn-sm">Add to Cart</button>
+          <div class="bd-example">
+          </div>
+        </div>
+      </div>
+  `;
+}
+
+$(document).ready(function() {
+  $('#mobile-products').html(mobileProducts(greenShades));
+});
